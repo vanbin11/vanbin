ffcd35a4aa9cf13787391f5f3877ee9c0e97cf43
vanbin <88774735+vanbin11@users.noreply.github.com>
2026-05-18 18:57:34 +0700

    psn.js

--- psn.js
+++ psn.js
@@ -38,9 +38,10 @@ const projects = [
   { title: "Quand l'art s'industrialise",
     date: 'September 20 2047', 
     d1: '...',
-    images: ['images/franc/franc-01.jpg', 'images/franc/franc-01.jpg', 'images/franc/franc-01.jpg',
-             'images/franc/franc-01.jpg', 'images/franc/franc-01.jpg', 'images/franc/franc-01.jpg',
-             'images/franc/franc-01.jpg', 'images/franc/franc-01.jpg', 'images/franc/franc-01.jpg',]
+    images: ['images/franc/franc-01.jpg', 'images/franc/franc-02.jpg', 'images/franc/franc-03.jpg',
+             'images/franc/franc-04.jpg', 'images/franc/franc-05.jpg', 'images/franc/franc-06.jpg',
+             'images/franc/franc-07.jpg', 'images/franc/franc-08.jpg', 'images/franc/franc-09.jpg',
+             'images/franc/franc-10.jpg', 'images/franc/franc-11.jpg'']
             },
   { title: 'Trảng Bàng Gourmet', 
     date: 'October 8 2047', 
